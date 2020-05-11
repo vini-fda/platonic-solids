b1a90125ce23e0ffc8332fe455371303b3a41506
vini-fda <viniciusc14h18n2o5@gmail.com>
2020-05-11 00:59:58 -0300

    Fixed bugs

--- index.js
+++ index.js
@@ -56,7 +56,6 @@ class Polihedron {
 
 function init() {
     scene = new THREE.Scene();
-    white_scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
     camera.position.z = 5;
     theta = 0;
